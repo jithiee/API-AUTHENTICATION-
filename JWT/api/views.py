2aa68e8f291294f79b9455abf60698a8caa9b7c3
jihtin <129176693+jihtin@users.noreply.github.com>
2023-11-02 17:20:28 +0530

    new commit

--- JWT/api/views.py
+++ JWT/api/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from . models import Student
+from .serializer import StudentSerializer
+# from rest_framework.authentication import 
+# from api.customAuth import CustomeAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
-# Create your views here.
+
+
+class StudentModelView(ModelViewSet):
+    queryset = Student.objects.all()
+    serializer = StudentSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+    
